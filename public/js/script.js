ab46a430a9a8cfe1a135e9174d1b4c28349e052d
James Davis <Jamesmdavis95@gmail.com>
2020-03-12 14:41:21 -0700

    Started filling in the information for Projects

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,12 @@
-const buttonPrevious = document.getElementById('button-previous');
-const buttonNext = document.getElementById('button-next');
+const arrowRight = document.getElementById('arrow-right');
+const arrowLeft = document.getElementById('arrow-left');
 const buttonProjects = document.getElementById('button-projects');
 const slider = document.getElementById('slides');
 const hr = document.getElementById('hr-project');
 
-const paginationListButtons = document.getElementById('pagination-list-buttons')
-    .getElementsByClassName('pagination-link');
-const slides = document.getElementsByClassName('transition');
+const slides = document.getElementsByClassName('box-container');
 
-let currentIndex = 0;
-let slideValue = 0;
+let currentSlide = 0;
 
 let currentTransform = -2/3 * 100;
 let incrementTransform = 1/3 * 100;
@@ -23,55 +20,27 @@ buttonProjects.addEventListener('click', () => {
     });
 });
 
-buttonNext.addEventListener('click', () => {
-    currentTransform = currentTransform + incrementTransform;
-    slider.style.transform = "translateX(" + currentTransform + "%)";
-
-    changeCurrentButton(true);
-    redefineButtons();
+arrowRight.addEventListener('click', () => {
+    if(currentSlide < slides.length - 1) {
+        currentTransform = currentTransform + incrementTransform;
+        slider.style.transform = "translateX(" + currentTransform + "%)";
+        currentSlide++;
+    }
 });
 
-buttonPrevious.addEventListener('click', () => {
-    currentTransform = currentTransform - incrementTransform;
-    slider.style.transform = "translateX(" + currentTransform + "%)";
-
-    changeCurrentButton(false);
-    redefineButtons();
+arrowLeft.addEventListener('click', () => {
+    if(currentSlide > 0) {
+        currentTransform = currentTransform - incrementTransform;
+        slider.style.transform = "translateX(" + currentTransform + "%)";
+        currentSlide--;
+    }
 });
 
 window.addEventListener('scroll', () => {
     let top = this.scrollY;
-    console.log(top);
 
     if(top >= 400) {
         hr.style.width = "300px";
         hr.style.opacity = "1";
     }
 });
-
-for(let i = 0; i < paginationListButtons.length; i++) {
-    paginationListButtons[i].addEventListener('click', () => {
-        slideValue = slideValue + ((i - currentIndex) * 100);
-        for(let slide of slides) {
-            slide.style.transform = "translateX(" + slideValue/3 + "%)";
-            console.log(slide);
-        }
-
-        paginationListButtons[currentIndex].classList.remove('is-current');
-        currentIndex = i;
-        paginationListButtons[i].classList.add('is-current');
-
-        redefineButtons();
-    });
-}
-
-function changeCurrentButton(forward) {
-    paginationListButtons[currentIndex].classList.remove('is-current');
-    currentIndex = forward ? currentIndex + 1 : currentIndex - 1;
-    paginationListButtons[currentIndex].classList.add('is-current');
-}
-
-function redefineButtons() {
-    buttonNext.disabled = currentIndex < paginationListButtons.length - 1 ? false : true;
-    buttonPrevious.disabled = currentIndex > 0 ? false : true;
-}
